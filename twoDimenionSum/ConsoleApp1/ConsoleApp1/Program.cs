a189522ef6cc7f5a563afca60676e9e77d0ee4bc
MaciejFur <mc.furdyna@gmail.com>
2019-07-21 09:41:12 +0200

    Update #4
    updated with program for two dimension list

---
+++ twoDimenionSum/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static int KtoraKolumna(int[,] a)
+        {
+            List<double> srednie = new List<double>(new double[a.GetLength(1)]);
+            
+
+            for (int i = 0; i < a.GetLength(0); ++i)
+            {
+                for (int j = 0; j < a.GetLength(1); ++j)
+                {
+                    srednie[j] += a[i, j];
+                }
+            }
+
+            for (int i = 0; i < srednie.Count(); ++i)
+            {
+                srednie[i] /= a.GetLength(0);
+            }
+
+            return srednie.IndexOf(srednie.Min());
+        }
+
+        public static void Main()
+        {
+            int[,] a = new int[5, 4] { { 1, 1, 1, 0 }, { -2, -3, 2, 1 }, { 2, -5, 0, -1 }, { -3, 4, 6, 2 }, { -6, 6, 3, 4 } };
+            Console.WriteLine("numer kolumny z najmniejszą sumą = " + KtoraKolumna(a));
+        }
+    }
+}
